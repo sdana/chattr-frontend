985bd7e45b357230bc3b2ffcd5086c260fa41d6b
Seth Dana <seth.dana@gmail.com>
2018-10-29 13:04:25 -0500

    Main page coming together. Top bar is working. Logout function is working.

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,26 +1,115 @@
-import React, {Component} from "react"
+import React from 'react';
+import { Redirect } from "react-router-dom"
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 import api from "../Api/Api"
 
-export default class MainPage extends Component {
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 
-    state = {
-        user: {}
-    }
+class MenuAppBar extends React.Component {
+  state = {
+    auth: true,
+    anchorEl: null,
+    user: {},
+    logout: false
+  };
 
-    componentDidMount() {
-        this.getUserDetails()
-    }
-    
-    getUserDetails = () => {
-        api.userDetails(sessionStorage.getItem("loginToken")).then(user => this.setState({user: user}))
-    }
+  componentDidMount = () => {
+    api.userDetails(sessionStorage.getItem("loginToken")).then(res => this.setState({user: res}))
+  }
 
-    render(){
+  logout = () => {
+    sessionStorage.removeItem("loginToken")
+    this.setState({logout: true})
+  }
 
-        return (
-            <React.Fragment>
-                <h1>Welcome to Chattr {this.state.user.email}</h1>
-            </React.Fragment>
-        )
-    }
+  handleChange = event => {
+    this.setState({ auth: event.target.checked });
+  };
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { auth, anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
+    return (
+      (this.state.logout) 
+      ? 
+      <Redirect to="/" /> 
+      :
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Welcome to Chattr {(this.state.user) ? `${this.state.user.firstName} ${this.state.user.lastName}` : ""}
+            </Typography>
+            {auth && (
+              <div>
+                <IconButton
+                  aria-owns={open ? 'menu-appbar' : null}
+                  aria-haspopup="true"
+                  onClick={this.handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={() => {
+                    this.handleClose()
+                    this.logout()
+
+                  }}>Logout</MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
+
+export default withStyles(styles)(MenuAppBar);
